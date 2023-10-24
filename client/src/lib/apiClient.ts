ae4f455f016ae69b68b23dc1254cbfc2ab9738b0
Takayuki0924 <takapon0924@icloud.com>
2023-10-24 10:46:32 +0900

    Update apiClient.ts

--- client/src/lib/apiClient.ts
+++ client/src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "http://172.16.1.31:8080/api/v1",
+    baseURL: "http://18.177.143.232:8080/api/v1",
     headers: {
         "Content-Type": "application/json"
     },
